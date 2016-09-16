896f13e08f8ad6bdcbaa44190f4d41aaa89e5a48
jdbancal <jdbancal.physics@gmail.com>
2016-09-16 10:15:54 +0200

    took care of NaN in logical conversion

--- @gem/logical.m
+++ @gem/logical.m
@@ -1,6 +1,10 @@
 % logical - returns true for nonzero elements
 function result = logical(this)
 
+if sum(isnan(this)) > 0
+    error('NaN''s cannot be converted to logicals.');
+end
+
 result = (this ~= 0);
 
 end
