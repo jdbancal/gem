0abcb7141e61c666452682817f5d58d5b5288e3d
Jean-Daniel Bancal <jdbancal@gmail.com>
2017-01-14 12:48:59 +0100

    corrected typo

--- @gem/disp.m
+++ @gem/disp.m
@@ -11,7 +11,7 @@ function disp(this, arg2)
     
     % Some versions of matlab provide the name as a second argument
     if (nargin >= 2)
-        displayPrecision = arg3;
+        displayPrecision = arg2;
     end
     
     gem_mex('display', this.objectIdentifier, displayPrecision);
