94b37e8233353452f89d95846d4987fc99830348
Jean-Daniel Bancal <jdbancal@gmail.com>
2017-06-05 15:21:28 +0200

    Support for additional options in sum function

--- @gem/sum.m
+++ @gem/sum.m
@@ -3,8 +3,20 @@
 % supported formats :
 %   sum(a) : column-wise sum
 %   sum(a, dim) : sum along the dimension dim
-function result = sum(this, dim)
-    % This function can involve up to two arguments
+%   sum(a, dim, type) : sum along the dimension dim and type can be any of
+%       the following:
+%        - double : the output will be recast in double type
+%        - native or default : the output is of gem type
+function result = sum(this, dim, type)
+    % This function can involve up to three arguments
+    if (nargin < 3)
+        type = 'native';
+    else
+        if ~isequal(type, 'double') && ~isequal(type, 'native') && ~isequal(type, 'default')
+            error('Unsupported argument in gem::sum');
+        end
+    end
+    
     if (nargin >= 2) && (iscell(dim) || (numel(dim) > 1) || (dim ~= 1 && dim ~= 2))
         error('Unexpected arguments in gem::sum');
     end
@@ -30,4 +42,10 @@ function result = sum(this, dim)
     
     % ...  and create a new matlab object to keep this handle
     result = gem('encapsulate', newObjectIdentifier);
+    
+    % If the user requested double output, we transform the output
+    % accordingly
+    if isequal(type, 'double')
+        result = double(result);
+    end
 end
--- @sgem/sum.m
+++ @sgem/sum.m
@@ -3,8 +3,20 @@
 % supported formats :
 %   sum(a) : column-wise sum
 %   sum(a, dim) : sum along the dimension dim
-function result = sum(this, dim)
-    % This function can involve up to two arguments
+%   sum(a, dim, type) : sum along the dimension dim and type can be any of
+%       the following:
+%        - double : the output will be recast in double type
+%        - native or default : the output is of sgem type
+function result = sum(this, dim, type)
+    % This function can involve up to three arguments
+    if (nargin < 3)
+        type = 'native';
+    else
+        if ~isequal(type, 'double') && ~isequal(type, 'native') && ~isequal(type, 'default')
+            error('Unsupported argument in sgem::sum');
+        end
+    end
+    
     if (nargin >= 2) && (iscell(dim) || (numel(dim) > 1) || (dim ~= 1 && dim ~= 2))
         error('Unexpected arguments in sgem::sum');
     end
@@ -27,4 +39,9 @@ function result = sum(this, dim)
             error('Unexpected argument in sgem::sum');
     end
     
+    % If the user requested double output, we transform the output
+    % accordingly
+    if isequal(type, 'double')
+        result = double(result);
+    end
 end
